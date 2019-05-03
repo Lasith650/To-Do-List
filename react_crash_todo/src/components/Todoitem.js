efe2fc0e6d2251d8fb90a795fa7048cb8a9f0306
lasith650 <lasith650@gmail.com>
2019-05-03 21:52:15 +0530

    Adding the delete button

--- react_crash_todo/src/components/Todoitem.js
+++ react_crash_todo/src/components/Todoitem.js
@@ -34,6 +34,7 @@ export class Todoitem extends Component {
         <p>
         <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
         {title}
+        <button style={btnStyle}>x</button>
         </p>
       </div>
     )
@@ -44,5 +45,15 @@ export class Todoitem extends Component {
 Todoitem.propTypes = {
     todo: PropTypes.object.isRequired
   }
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 10px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
 
 export default Todoitem
