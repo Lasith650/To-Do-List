098e0b22b9b3d4346ca1ad95ba4425a7b78c9aaf
lasith650 <lasith650@gmail.com>
2019-05-03 13:01:05 +0530

    Added Todoitem.js

---
+++ react_crash_todo/src/components/Todoitem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Todoitem extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello everyone. I love this country!!!</p>
+      </div>
+    )
+  }
+}
+
+export default Todoitem
