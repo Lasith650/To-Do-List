63c01323b02715740a497eb13ecc97aa7f7041d3
lasith650 <lasith650@gmail.com>
2019-05-04 14:32:40 +0530

    Adding Router to App.js

--- react_crash_todo/src/App.js
+++ react_crash_todo/src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
@@ -54,13 +55,15 @@ class App extends Component{
 
   render(){
   return (
-    <div className="App">
-      <div className="container">
-        <Header/>
-        <AddTodo addTodo={this.addTodo}/>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delToDo={this.delToDo}/>
+    <Router>
+      <div className="App">
+        <div className="container">
+          <Header/>
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delToDo={this.delToDo}/>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 }
